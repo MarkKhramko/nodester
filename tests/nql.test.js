6958bfbe68f3cec3fd012f972a7519c8df73c276
Mark Khramko <markkhramko@gmail.com>
2023-12-08 03:12:08 +0100

    [tests] Refactor nql test.

--- tests/nql.test.js
+++ tests/nql.test.js
@@ -10,285 +10,354 @@ const {
 const { ModelsTree } = require('../lib/middlewares/ql/sequelize/interpreter/ModelsTree');
 const QueryLexer = require('../lib/middlewares/ql/sequelize/interpreter/QueryLexer');
 
+
+
 describe('nodester Query Language', () => {
-	const queryStrings = [
-		// Simple where.
-		'id=10',
-		// All possible params.
-		'id=10&position=4&limit=3&skip=10&order=desc&order_by=index&fields=id,content,position,created_at',
-		// Simple includes.
-		'includes=comments&id=7',
-		// Include with All possible params.
-		'includes=comments(id=10&position=4&limit=3&skip=10&order=desc&order_by=index&fields=id,content,position)',
-		
-		// Subinclude horizontal.
-		'includes=comments,users&id=1000',
-		// Subinclude horizontal (more entries).
-		'includes=comments(order=desc),users,likes(order=rand),reposts&id=1000',
-		// Subinclude horizontal (+ syntaxis).
-		'includes=comments(order=desc).users+likes(order=rand&order_by=position)&id=1000',
+	describe('flat', () => {
+		const queryStrings = [
+			// Simple where.
+			'id=10',
+			// All possible params.
+			'id=10&position=4&limit=3&skip=10&order=desc&order_by=index&fields=id,content,position,created_at',
+		];
 
-		// Subinclude vertical.
-		'includes=comments.users&id=1000',
-		// Subinclude vertical (more entries).
-		'in=comments.users.avatars.sizes&position=200',
+		it('Simple where', () => {
+			const lexer = new QueryLexer( queryStrings[0] );
+			const result = lexer.query;
 
-		// Complex includes.
-		'includes=comments.users.avatars(fields=id,content&order=rand)&id=7&limit=3',
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['10'] });
+			const expected = tree.root.toObject();
 
-		// Broken includes.
-		'includes=comments(order=rand)&id=7&limit=3&includes=users(fields=id,content)',
+			expect(result).toMatchObject(expected);
+		});
 
-		// OR simple.
-		'or(index=2,position=5)',
-		// OR shortened.
-		'|(index=2,position=5)',
+		test('All possible params', () => {
+			const lexer = new QueryLexer( queryStrings[1] );
+			const result = lexer.query;
 
-		// NOT inside include.
-		'includes=comments(id=not(7))',
 
-		// Like simple.
-		'title=like(some_text)',
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['10'], position: ['4'] });
+			tree.node.fields = [ 'id', 'content', 'position', 'created_at' ];
+			tree.node.limit = 3;
+			tree.node.skip = 10;
+			tree.node.order = 'desc';
+			tree.node.order_by = 'index';
+			const expected = tree.root.toObject();
 
-		// Subinclude and isolated Horizontal.
-		'in=comments.user,likes',
-	];
-
-	it('query "Simple where"', () => {
-		const lexer = new QueryLexer( queryStrings[0] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['10'] });
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
+			expect(result).toMatchObject(expected);
+		});
 	});
 
-	test('query "All possible params"', () => {
-		const lexer = new QueryLexer( queryStrings[1] );
-		const result = lexer.query;
+	describe('includes', () => {
+		const queryStrings = [
+			// Simple includes.
+			'includes=comments&id=7',
+			// Include with All possible params.
+			'includes=comments(id=10&position=4&limit=3&skip=10&order=desc&order_by=index&fields=id,content,position)',
+
+			// 2 horizontals
+			'includes=comments,users&id=1000',
+			
+			// Horizontals queried.
+			'includes=comments(order=desc),users,likes(order=rand),reposts&id=1000',
+			// Horizontals queried №2.
+			'in=reactions,comments(user_id=gte(4)&skip=10&limit=2).users,likes,reposts',
+
+			// Separated includes.
+			'includes=comments(order=rand)&id=7&limit=3&includes=users(fields=id,content)',
+		];
+
+		test('Simple includes', () => {
+			const lexer = new QueryLexer( queryStrings[0] );
+			const result = lexer.query;
 
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['10'], position: ['4'] });
-		tree.node.fields = [ 'id', 'content', 'position', 'created_at' ];
-		tree.node.limit = 3;
-		tree.node.skip = 10;
-		tree.node.order = 'desc';
-		tree.node.order_by = 'index';
-		const expected = tree.root.toObject();
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['7'] });
+			tree.include('comments');
+			const expected = tree.root.toObject();
 
-		expect(result).toMatchObject(expected);
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Include with all possible params', () => {
+			const lexer = new QueryLexer( queryStrings[1] );
+			const result = lexer.query;
+
+			const tree = new ModelsTree();
+			tree.include('comments').use('comments');
+			tree.node.addWhere({ id: ['10'], position: ['4'] });
+			tree.node.fields = [ 'id', 'content', 'position' ];
+			tree.node.limit = 3;
+			tree.node.skip = 10;
+			tree.node.order = 'desc';
+			tree.node.order_by = 'index';
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('2 horizontals', () => {
+			const lexer = new QueryLexer( queryStrings[2] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['1000'] });
+			tree.include('comments');
+			tree.include('users');
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Horizontals queried', () => {
+			const lexer = new QueryLexer( queryStrings[3] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['1000'] });
+			tree.include('comments').use('comments');
+			tree.node.order = 'desc';
+			tree.up();
+			tree.include('users');
+			tree.include('likes') && tree.use('likes');
+			tree.node.order = 'rand';
+			tree.up();
+			tree.include('reposts');
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Horizontals queried №2', () => {
+			const lexer = new QueryLexer( queryStrings[4] );
+			const result = lexer.query;
+
+			const tree = new ModelsTree();
+			tree.include('reactions');
+
+			tree.include('comments').use('comments');
+			tree.node.addWhere({
+					user_id: {
+						gte: ['4']
+					}
+			});
+			tree.node.skip = 10;
+			tree.node.limit = 2;
+
+			tree.include('users');
+			tree.up();
+
+			tree.include('likes');
+			tree.include('reposts');
+
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Separated includes"', () => {
+			const lexer = new QueryLexer( queryStrings[5] );
+			const result = lexer.query;
+
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['7'] });
+			tree.node.limit = 3;
+			tree.include('comments').use('comments');
+			tree.node.order = 'rand';
+			tree.up();
+			tree.include('users').use('users');
+			tree.node.fields = [ 'id', 'content' ];
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
 	});
 
-	test('query "Simple includes"', () => {
-		const lexer = new QueryLexer( queryStrings[2] );
-		const result = lexer.query;
+	describe('subincludes', () => {
+		const queryStrings = [
+			// Simple subinclude.
+			'includes=comments.users',
+
+			// Deep subincludes.
+			'in=posts.comments.users.avatars.sizes&position=200',
+
+			// Simple horizontal subinclude, "+" syntaxis.
+			'includes=comments.users+likes',
+
+			// Subinclude query.
+			'includes=comments.users(order=rand&order_by=position)',
+
+			// Complex subincludes query, "+" syntaxis.
+			'includes=comments(order=desc).users+likes(order=rand&order_by=position)&id=1000',
+		];
+
+		test('Simple subinclude', () => {
+			const lexer = new QueryLexer( queryStrings[0] );
+			const result = lexer.query;
 
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['7'] });
-		tree.include('comments');
-		const expected = tree.root.toObject();
+			const tree = new ModelsTree();
+			tree.include('comments').use('comments');
+			tree.include('users');
+			const expected = tree.root.toObject();
 
-		expect(result).toMatchObject(expected);
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Deep subincludes', () => {
+			const lexer = new QueryLexer( queryStrings[1] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.include('posts').use('posts');
+			tree.include('comments').use('comments');
+			tree.include('users').use('users');
+			tree.include('avatars').use('avatars');
+			tree.include('sizes').use('sizes');
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Simple horizontal subinclude, "+" syntaxis"', () => {
+			const lexer = new QueryLexer( queryStrings[2] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.include('comments').use('comments');
+			tree.include('users');
+			tree.include('likes');
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Subinclude query', () => {
+			const lexer = new QueryLexer( queryStrings[3] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.include('comments').use('comments');
+			tree.include('users').use('users');
+			tree.node.order = 'rand';
+			tree.node.order_by = 'position';
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
+
+		test('Complex subincludes query, "+" syntaxis', () => {
+			const lexer = new QueryLexer( queryStrings[4] );
+			const result = lexer.query;
+
+
+			const tree = new ModelsTree();
+			tree.node.addWhere({ id: ['1000'] });
+			tree.include('comments').use('comments');
+			tree.node.order = 'desc';
+			tree.include('users');
+			tree.include('likes') && tree.use('likes');
+			tree.node.order = 'rand';
+			tree.node.order_by = 'position';
+			tree.up();
+			const expected = tree.root.toObject();
+
+			expect(result).toMatchObject(expected);
+		});
 	});
 
-	test('query "Include with all possible params"', () => {
-		const lexer = new QueryLexer( queryStrings[3] );
-		const result = lexer.query;
+	describe('operations', () => {
+		const queryStrings = [
+			// OR simple.
+			'or(index=2,position=5)',
+			// OR short.
+			'|(index=2,position=5)',
 
-		const tree = new ModelsTree();
-		tree.include('comments').use('comments');
-		tree.node.addWhere({ id: ['10'], position: ['4'] });
-		tree.node.fields = [ 'id', 'content', 'position' ];
-		tree.node.limit = 3;
-		tree.node.skip = 10;
-		tree.node.order = 'desc';
-		tree.node.order_by = 'index';
-		const expected = tree.root.toObject();
+			// Not simple.
+			'key=not(main)',
+			// Not short.
+			'key=!(main)',
+			// NOT inside include.
+			'includes=comments(id=not(7))',
 
-		expect(result).toMatchObject(expected);
-	});
+			// Like simple.
+			'title=like(some_text)',
+		];
 
+		test('"OR" simple', () => {
+			const lexer = new QueryLexer( queryStrings[0] );
+			const result = lexer.query;
 
-	test('query "Subinclude horizontal"', () => {
-		const lexer = new QueryLexer( queryStrings[4] );
-		const result = lexer.query;
+			const tree = new ModelsTree();
+			tree.node.addWhere({ or: [ { index: ['2'] }, { position: ['5'] } ] });
+			const expected = tree.root.toObject();
 
+			expect(result).toMatchObject(expected);
+		});
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['1000'] });
-		tree.include('comments');
-		tree.include('users');
-		const expected = tree.root.toObject();
+		test('"OR" short', () => {
+			const lexer = new QueryLexer( queryStrings[1] );
+			const result = lexer.query;
 
-		expect(result).toMatchObject(expected);
-	});
+			const tree = new ModelsTree();
+			tree.node.addWhere({ or: [ { index: ['2'] }, { position: ['5'] } ] });
+			const expected = tree.root.toObject();
 
-	test('query "Subinclude horizontal (complex)"', () => {
-		const lexer = new QueryLexer( queryStrings[5] );
-		const result = lexer.query;
+			expect(result).toMatchObject(expected);
+		});
 
+		test('"NOT" simple', () => {
+			const lexer = new QueryLexer( queryStrings[2] );
+			const result = lexer.query;
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['1000'] });
-		tree.include('comments').use('comments');
-		tree.node.order = 'desc';
-		tree.up();
-		tree.include('users');
-		tree.include('likes') && tree.use('likes');
-		tree.node.order = 'rand';
-		tree.up();
-		tree.include('reposts');
-		const expected = tree.root.toObject();
+			const tree = new ModelsTree();
+			tree.node.addWhere({ key: { not: ['main'] } });
+			const expected = tree.root.toObject();
 
-		expect(result).toMatchObject(expected);
-	});
+			expect(result).toMatchObject(expected);
+		});
 
+		test('"NOT" short', () => {
+			const lexer = new QueryLexer( queryStrings[3] );
+			const result = lexer.query;
 
-	test('query "Subinclude horizontal (+ syntaxis)"', () => {
-		const lexer = new QueryLexer( queryStrings[6] );
-		const result = lexer.query;
+			const tree = new ModelsTree();
+			tree.node.addWhere({ key: { not: ['main'] } });
+			const expected = tree.root.toObject();
 
+			expect(result).toMatchObject(expected);
+		});
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['1000'] });
-		tree.include('comments').use('comments');
-		tree.node.order = 'desc';
-		tree.include('users');
-		tree.include('likes') && tree.use('likes');
-		tree.node.order = 'rand';
-		tree.node.order_by = 'position';
-		tree.up();
-		const expected = tree.root.toObject();
+		test('"NOT" inside includes', () => {
+			const lexer = new QueryLexer( queryStrings[4] );
+			const result = lexer.query;
 
-		expect(result).toMatchObject(expected);
-	});
+			const tree = new ModelsTree();
+			tree.include('comments').use('comments');
+			tree.node.addWhere({ id: { not: ['7'] }});
+			const expected = tree.root.toObject();
 
+			expect(result).toMatchObject(expected);
+		});
 
-	test('query "Subinclude vertical"', () => {
-		const lexer = new QueryLexer( queryStrings[7] );
-		const result = lexer.query;
+		test('"Like" simple', () => {
+			const lexer = new QueryLexer( queryStrings[5] );
+			const result = lexer.query;
 
+			const tree = new ModelsTree();
+			tree.node.addWhere({ title: { like: ['some_text'] }});
+			const expected = tree.root.toObject();
 
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['1000'] });
-		tree.include('comments').use('comments');
-		tree.include('users');
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('query "Subinclude vertical (complex)"', () => {
-		const lexer = new QueryLexer( queryStrings[8] );
-		const result = lexer.query;
-
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ position: ['200'] });
-		tree.include('comments').use('comments');
-		tree.include('users').use('users');
-		tree.include('avatars').use('avatars');
-		tree.include('sizes').use('sizes');
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('query "Complex includes"', () => {
-		const lexer = new QueryLexer( queryStrings[9] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['7'] });
-		tree.node.limit = 3;
-		tree.include('comments').use('comments');
-		tree.include('users').use('users');
-		tree.include('avatars').use('avatars');
-		tree.node.fields = [ 'id', 'content' ];
-		tree.node.order = 'rand';
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('query "Broken includes"', () => {
-		const lexer = new QueryLexer( queryStrings[10] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ id: ['7'] });
-		tree.node.limit = 3;
-		tree.include('comments').use('comments');
-		tree.node.order = 'rand';
-		tree.up();
-		tree.include('users').use('users');
-		tree.node.fields = [ 'id', 'content' ];
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('Token "OR" simple', () => {
-		const lexer = new QueryLexer( queryStrings[11] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ or: [ { index: ['2'] }, { position: ['5'] } ] });
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('Token "OR" shortened', () => {
-		const lexer = new QueryLexer( queryStrings[12] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ or: [ { index: ['2'] }, { position: ['5'] } ] });
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('Token "NOT"', () => {
-		const lexer = new QueryLexer( queryStrings[13] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.include('comments').use('comments');
-		tree.node.addWhere({ id: { not: ['7'] }});
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	test('Token "Like" simple', () => {
-		const lexer = new QueryLexer( queryStrings[14] );
-		const result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.node.addWhere({ title: { like: ['some_text'] }});
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
-	});
-
-	it('query "Subinclude and isolated Horizontal"', () => {
-		const lexer = new QueryLexer( queryStrings[15] );
-		result = lexer.query;
-
-		const tree = new ModelsTree();
-		tree.include('comments').use('comments');
-		tree.include('user');
-		tree.up();
-		tree.include('likes');
-		const expected = tree.root.toObject();
-
-		expect(result).toMatchObject(expected);
+			expect(result).toMatchObject(expected);
+		});
 	});
 
 });
