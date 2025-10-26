4447be6b4ee7f8986609ef535e15d7df7b0a31cb
Mark Khramko <markkhramko@gmail.com>
2025-10-26 19:08:38 +0100

    [examples/boilerplate] Add creation of additional directories in the Bootstrap step 2

--- examples/boilerplate/bootstrap/init/steps/2.js
+++ examples/boilerplate/bootstrap/init/steps/2.js
@@ -15,13 +15,35 @@ module.exports = {
 async function _run() {
 	console.info('Step 2 performing...');
 
+	// ---- Controllers ----
 	const controllersPath = nodesterConfig.paths.controllers;
 
 	const controllersDirectoryExists = fsSync.existsSync(controllersPath);
 	if (!controllersDirectoryExists) {
 		await fs.mkdir(controllersPath);
 	}
-	console.info('✅ controllers directory created');
+	console.info('✅ "controllers" directory created\n');
+
+
+	// ---- Facades ----
+	const facadesPath = nodesterConfig.paths.facades;
+
+	const facadesDirectoryExists = fsSync.existsSync(facadesPath);
+	if (!facadesDirectoryExists) {
+		await fs.mkdir(facadesPath);
+	}
+	console.info('✅ "facades" directory created\n');
+
+
+	// ---- filters ----
+	const filtersPath = nodesterConfig.paths.filters;
+
+	const filtersDirectoryExists = fsSync.existsSync(filtersPath);
+	if (!filtersDirectoryExists) {
+		await fs.mkdir(filtersPath);
+	}
+	console.info('✅ "filters" directory created\n');
+
 
 	console.info('\n');
 	return Promise.resolve();
